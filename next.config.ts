11756867e5c13594730a722ca3d6dd794d9f62e0
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2025-08-28 17:12:15 +0530

    fix:disable ESLint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
+  images: {
+    domains: ["res.cloudinary.com"], // allow Cloudinary images
+  },
 };
 
 export default nextConfig;
