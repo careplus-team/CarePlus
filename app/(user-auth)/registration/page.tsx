bfb8eae938e53e057b9b2b79f5f66092a15242ae
D.A.Pasindu rasanga <s23010166@ousl.lk>
2025-10-01 18:18:18 +0530

    feat(doctor-reg):add doctor registration

---
+++ app/(user-auth)/registration/page.tsx
@@ -0,0 +1,12 @@
+import DoctorRegistrationComponent from '@/components/non-user-auth/doctor-reg';
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <DoctorRegistrationComponent/>
+    </div>
+  )
+}
+
+export default page;
