5e0c370bc1836961933f788c9aee76bfc996bdd5
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2026-01-12 23:15:06 +0530

    fix:type script error fixed

--- lib/zod-schema/signup.schema.ts
+++ lib/zod-schema/signup.schema.ts
@@ -20,9 +20,7 @@ export const SignUpSchema = z
       .min(6, "Confirm Password must be at least 6 characters long")
       .max(100, "Confirm Password must be at most 100 characters long"),
     age: z.number().min(0, "Age must be a positive number").optional(),
-    gender: z.enum(["male", "female", "other"]).refine((val) => val !== "", {
-      message: "Gender is required",
-    }),
+    gender: z.enum(["male", "female", "other"]),
     username: z
       .string()
       .min(1, "Username is required")
