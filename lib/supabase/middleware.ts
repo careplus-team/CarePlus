2b89fa2532b779bc5dbead9ebe84945f4edb40af
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2026-01-13 00:02:30 +0530

    fix:minor bug fixed

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -60,10 +60,7 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/registration") &&
     !request.nextUrl.pathname.startsWith("/admin/admin-login") &&
     !request.nextUrl.pathname.startsWith("/doctor/doctor-login") &&
-    !request.nextUrl.pathname.startsWith("/doctor/doctor-dashboard") &&
     !request.nextUrl.pathname.startsWith("/doctor/doctor-registration") &&
-    !request.nextUrl.pathname.startsWith("/doctor/patient-list-view") &&
-    !request.nextUrl.pathname.startsWith("/admin/create-channel") &&
     !request.nextUrl.pathname.startsWith("/emergency-manager-login") &&
     !request.nextUrl.pathname.startsWith("/ambulance-login")
   ) {
